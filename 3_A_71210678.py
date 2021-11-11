95fe388d5f59161b1aa06b3ef6928bf823662fcc
SetyaKC <93339520+SetyaKC@users.noreply.github.com>
2021-11-11 22:47:45 +0700

    file jawaban soal 3

---
+++ 3_A_71210678.py
@@ -0,0 +1,9 @@
+#fitur untuk menambah catatan di progam Doni
+
+barang1 = input("Masukkan daftar belanja 1: ")
+barang2 = input("Masukkan daftar belanja 2: ")
+barangnambah1 = input("Tambahkan data ke daftar belanja 1: ")
+barangnambah2 = input("Tambahkan data ke daftar belanja 2: ")
+
+print("Daftar belanja 1 adalah", barang1 +","+barangnambah1)
+print("Daftar belanja 2 adalah", barang2 +","+barangnambah1)
