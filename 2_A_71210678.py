07cfa79653367040ffd5dad2bf3c49f64b1f8ac2
SetyaKC <93339520+SetyaKC@users.noreply.github.com>
2021-11-11 22:46:40 +0700

    file jawaban soal 2

---
+++ 2_A_71210678.py
@@ -0,0 +1,7 @@
+#menghitung perpangkatan lima
+
+bilangan = int(input("Masukkan angka: "))
+besarpangkat = 5
+
+hasil = pow(bilangan, besarpangkat)
+print(bilangan,"pangkat",besarpangkat,"adalah",hasil)
